04b0f7e1e65c11453d6073db6301ef03f18ad4a8
Christian Boehme <c.b.90@gmx.de>
2021-05-19 17:59:40 +0200

    done for today

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+  before_action :set_model, only: [:show, :edit]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def edit
+      
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
