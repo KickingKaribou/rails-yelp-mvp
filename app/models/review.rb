899c9b74184cb53d8c70a1f3dc97032bc387f8bd
Christian Boehme <c.b.90@gmx.de>
2021-05-19 15:50:32 +0200

    lets go

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true, less_than: 6, greater_than: -1 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+puts 'Creating restaurants...'
+tour_d_argent = Restaurant.new(name: "La Tour d'Argent", address: 'Lyon', category: 'french')
+tour_d_argent.save!
+
+chez_gladines = Restaurant.new(name: 'Chez Gladines', address: 'Paris', category: 'french')
+chez_gladines.save!
+
+would_you = Restaurant.new(name: 'Would you like fries?', address: 'Antwerpen', category: 'belgian')
+would_you.save!
+
+sakana = Restaurant.new(name: 'Sakana', address: 'Düsseldorf', category: 'japanese')
+sakana.save!
+
+rourourou = Restaurant.new(name: 'Rou Rou Rou', address: 'London', category: 'chinese')
+rourourou.save!
+
+trattoria_emanuele = Restaurant.new(name: 'Trattoria Emanuele', address: 'Milano', category: 'italian')
+trattoria_emanuele.save!
+puts 'Finished!'
